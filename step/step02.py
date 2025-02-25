e5fd5b47d8748d75bc822b859bca83b55d8bd6fc
wuhusie <whf131313@163.com>
2025-02-25 18:09:21 +0800

    add step/step02.py

---
+++ step/step02.py
@@ -0,0 +1,20 @@
+class Function:
+    def __call__(self, input):
+        '''
+        input: Variable
+        output: Variable
+
+        Variable是一个包含data和grad的类
+        '''
+        x = input.data
+        y = self.forward(x) # 具体的计算在forward方法中实现
+        output = Variable(y)
+        return output
+    
+    def forward(self, x):
+        '''
+        具体的计算，由子类实现。
+        这里抛出异常，要求子类必须实现forward方法，否则会报错
+        这是一种设计模式，叫做模板设计模式，即定义一个算法的骨架，而将一些步骤延迟到子类中实现，使得子类可以不改变一个算法的结构即可重定义该算法的某些特定步骤。
+        '''
+        raise NotImplementedError()
